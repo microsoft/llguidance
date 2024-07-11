cadabdad21f3b81ff58b1918f8c23116b4ff7af3
Michal Moskal <michal@moskal.me>
2024-07-10 17:37:53 -0700

    allow side-by side tests

--- scripts/test-guidance.sh
+++ scripts/test-guidance.sh
@@ -6,13 +6,22 @@ cd $(dirname $0)/..
 pip uninstall -y llguidance || :
 pip install -e . -v --no-build-isolation
 
-mkdir -p tmp
-cd tmp
-if test -f guidance/tests/unit/test_ll.py ; then
-   echo "Guidance clone OK"
+PYTEST_FLAGS=
+
+if test -f ../guidance/tests/unit/test_ll.py ; then
+    echo "Guidance side by side"
+    cd ../guidance
 else
-    git clone -b lazy_grammars https://github.com/hudson-ai/guidance
+    mkdir -p tmp
+    cd tmp
+    if test -f guidance/tests/unit/test_ll.py ; then
+    echo "Guidance clone OK"
+    PYTEST_FLAGS=-v
+    else
+        git clone -b lazy_grammars https://github.com/hudson-ai/guidance
+    fi
+    cd guidance
 fi
-cd guidance
-python -m pytest -v tests/unit/test_ll.py # main test
-python -m pytest -v tests/unit/test_[lgmp]*.py tests/unit/library
+
+python -m pytest $PYTEST_FLAGS tests/unit/test_ll.py # main test
+python -m pytest $PYTEST_FLAGS tests/unit/test_[lgmp]*.py tests/unit/library
