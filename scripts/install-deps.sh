9864e5dcdfbc4c16167a910a750fc22f272aaffe
Michal Moskal <michal@moskal.me>
2024-07-19 09:21:37 -0700

    build with zig

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # installing guidance for deps
-pip install pytest guidance huggingface_hub tokenizers jsonschema maturin
+pip install pytest guidance huggingface_hub tokenizers jsonschema maturin[zig]
 pip uninstall -y guidance
