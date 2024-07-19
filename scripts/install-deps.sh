3e742aae572e281ea5c2ffb5889df344dbcf41c5
Michal Moskal <michal@moskal.me>
2024-07-18 17:14:15 -0700

    fixing dist build

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # installing guidance for deps
-pip install setuptools==68 setuptools-rust pytest guidance huggingface_hub tokenizers jsonschema wheel
+pip install setuptools==68 setuptools-rust pytest guidance huggingface_hub tokenizers jsonschema wheel twine build
 pip uninstall -y guidance
 
