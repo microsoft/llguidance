85c7f8f9efdca04db1ecc5cf03f11190c59170fa
v-jkegler <v-jkegler@microsoft.com>
2024-09-20 16:15:42 -0400

    Add comment to scan() (#12)
    Begin the comment for scan()

--- parser/src/earley/parser.rs
+++ parser/src/earley/parser.rs
@@ -1100,7 +1100,17 @@ impl ParserState {
         }
         true
     }
+    
+    // scan() implements the version of Earley described in the
+    // slides for "Parsing: Earley parsing", Winter 2017/2018,
+    // Laura Kallmeyer, Heinrich Heine Universitaet, Dusseldorf,
+    // https://user.phil-fak.uni-duesseldorf.de/~kallmeyer/Parsing/earley.pdf
+    // (Retrieved 18 Sep 2024).
 
+    // An important difference between the algorithm implemented here
+    // and Kallmeyer's is that in scan(), the token scan is performed
+    // first, while in Kallmeyer it is performed last.
+    
     // lexeme body only used for captures (in definitive mode)
     // and debugging (lexeme.idx used always)
     fn scan(&mut self, lexeme: &Lexeme) -> bool {
