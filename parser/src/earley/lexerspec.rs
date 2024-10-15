b879e3fee677d4efc886934eb6a8a394b559d313
Jeffrey Kegler <jeffreykegler.notices@protonmail.com>
2024-10-15 18:34:42 -0400

    Add SKIP lexeme comments

--- parser/src/earley/lexerspec.rs
+++ parser/src/earley/lexerspec.rs
@@ -32,6 +32,9 @@ pub struct LexemeSpec {
 pub struct LexemeIdx(usize);
 
 impl LexemeIdx {
+    // SKIP is a pseudo-lexeme, a "no-op" which it is sometimes
+    // convenient to insert in the lexeme stream.  As the name
+    // suggests, it is skipped in parsing.
     pub const SKIP: LexemeIdx = LexemeIdx(0);
 
     pub fn new(idx: usize) -> Self {
