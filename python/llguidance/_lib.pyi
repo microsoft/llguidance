4954b4167ed3e28de6bfeacf7447d97b4a4c15a9
Hudson Cooper <hncooper96@gmail.com>
2024-11-21 14:13:26 -0800

    fix the pyi to reflect the correct signature of JsonCompiler

--- python/llguidance/_lib.pyi
+++ python/llguidance/_lib.pyi
@@ -143,7 +143,8 @@ class LLInterpreter:
 class JsonCompiler:
     def __new__(
         cls,
-        compact: bool = False,
+        separators: Optional[tuple[str, str]] = None,
+        whitespace_flexible: bool = False,
     ) -> "JsonCompiler":
         """
         Create a new JSON compiler.
