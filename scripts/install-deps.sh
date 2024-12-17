c39cbdc20668fe9c7ed934cde3ce5e102c66668b
Michal Moskal <michal@moskal.me>
2024-12-17 10:07:07 +0100

    print versions

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -4,3 +4,7 @@
 pip install pytest guidance huggingface_hub tokenizers jsonschema maturin[zig] \
     torch transformers bitsandbytes ipython
 pip uninstall -y guidance
+
+# print out versions
+rustc --version
+python --version
